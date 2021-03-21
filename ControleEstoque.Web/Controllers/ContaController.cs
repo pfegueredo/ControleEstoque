50855b165b8f498493641dd332e4fc8aa40b4a46
unknown <paulofegueredo@gmail.com>
2021-03-21 19:59:56 -0300

    Controle de Estoque

---
+++ ControleEstoque.Web/Controllers/ContaController.cs
@@ -0,0 +1,49 @@
+﻿using ControleEstoque.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ControleEstoque.Web.Controllers
+{
+    public class ContaController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel login, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+
+            var achou = (login.Usuario == "paulo" && login.Senha == "1234");
+
+            if (achou)
+            {
+                FormsAuthentication.SetAuthCookie(login.Usuario, login.LembrarMe);
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Login inválido");
+            }
+            return View(login);
+        }
+    }
+}
